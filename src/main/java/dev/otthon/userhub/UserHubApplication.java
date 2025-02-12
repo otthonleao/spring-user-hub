d017f3e8ad72357c78799f71d22acaadb9219aed
Otthon Leão <otthonleao@hotmail.com>
2025-02-12 16:38:51 -0400

    initial commit

---
+++ src/main/java/dev/otthon/userhub/UserHubApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.userhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/otthon/userhub/UserHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.userhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
